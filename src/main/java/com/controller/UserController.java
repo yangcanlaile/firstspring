87e0111bdc04cfe0bdda03e0081517481a86b09d
yangcanlaile <492573329@qq.com>
2019-10-18 14:58:29 +0800

    add

--- src/main/java/com/controller/UserController.java
+++ src/main/java/com/controller/UserController.java
@@ -1,14 +1,14 @@
 package com.controller;
 
-import com.alibaba.fastjson.JSON;
-import com.alibaba.fastjson.serializer.SerializerFeature;
 import com.entity.User;
 import com.service.UserServiceImp;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import javax.annotation.Resource;
+import java.util.List;
 
 @RestController
 @RequestMapping("api/hello")
@@ -17,9 +17,15 @@ public class UserController {
     @Resource
     private UserServiceImp userServiceImp;
 
+    @Autowired
+    private List<User> userList;
+
     @GetMapping
     public String Get() {
-        User user = userServiceImp.selectByPrimaryKey(1);
-        return JSON.toJSONString(user, SerializerFeature.WriteMapNullValue);
+        String outStr = "";
+        for (User user : userList) {
+            outStr += user.getUserName() + ": " + user.getPassword()+"<br//>";
+        }
+        return outStr;
     }
 }
